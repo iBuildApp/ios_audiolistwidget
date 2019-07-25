5b9dc7f8d39df2524f59cd922bf6eb381146ebc6
Anton Boyarkin <anton.boyarkin@icloud.com>
2019-07-25 15:16:48 +0300

    Update to New JSON Format

--- Sources/AudioListModuleRouter.swift
+++ Sources/AudioListModuleRouter.swift
@@ -23,7 +23,7 @@ public class AudioListModuleRouter: BaseRouter<AudioListModuleRoute> {
     }
 
     public override func prepareTransition(for route: AudioListModuleRoute) -> RouteTransition {
-        return RouteTransition(module: generateRootViewController(), isAnimated: true, showNavigationBar: true, showTabBar: false)
+        return RouteTransition(module: generateRootViewController(), isAnimated: true)
     }
 
     public override func rootTransition() -> RouteTransition {
